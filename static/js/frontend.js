a39a3af441bbe76debe9c6475eb1f87509a06638
Владислав Перегудов <peregudow.2016@yandex.ru>
2021-02-01 13:03:16 +0300

    pasha

--- static/js/frontend.js
+++ static/js/frontend.js
@@ -1,7 +1,34 @@
+var intervalPointer;
+
 function router(state) {
-console.log(document.location.hash);
-test();
+var route = document.location.hash.replace('#', '');
+switch(route){
+	case 'login':
+	document.getElementById("app").innerHTML = 'Здесь будет форма логина';
+	break;
+	case 'list':
+	document.getElementById("app").innerHTML = 'Здесь будет форма списка';
+	break;
+	default:
+	document.getElementById("app").innerHTML = 'Здесь будет форма списка';
+	break;
+}
+clearInterval(intervalPointer);
+intervalPointer = setInterval(function() {
+	var newRoute;
+	if(route === 'login'){
+		newRoute = 'list';
+			}	else if(route === 'list')  {
+				newRoute = 'login';
+			} else {
+				newRoute= 'list'
+			}
+			document.location.hash = `#${newRoute}`;
+}, 1000);
 }
 
 window.addEventListener('popstate', router);
 window.addEventListener('load', router);
+
+
+
