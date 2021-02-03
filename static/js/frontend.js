82c2fef158f070577e6d01635ae8d3802c9f2f31
Vladislav <peregudow.2016@yandex.ru>
2021-02-03 22:09:55 +0300

    03.02 Добавлена пакетная обработка

--- static/js/frontend.js
+++ static/js/frontend.js
@@ -1,6 +1,3 @@
-var intervalPointer;
-
-// client routes:
 var handlers = {};
 
 handlers['login'] = login;
@@ -21,4 +18,23 @@ window.addEventListener('load', router);
 window.addEventListener('load', function(){
 	var btnLogout = document.getElementById('btn-logout');
 	btnLogout.addEventListener('click', doLogout);
+
+	if(localStorage.getItem('login')){
+		document.getElementById('user-name-area').innerHTML = `Вы вошли как: <b>${localStorage.getItem('login')}</b>`;
+	}
+
+	document.getElementById('modal-dialog-close-btn').addEventListener('click', function(){
+
+		var dlg = document.getElementById('modal-dialog');
+		var dlgImg = document.getElementById('modal-dialog-img');
+		var dlgTitle = document.getElementById('modal-dialog-title');
+		dlgTitle.innerHTML = '';
+		dlgImg.setAttribute('src', '');
+		console.log('classList before?', dlg.classList);
+		dlg.classList.remove('show');
+		dlg.classList.add('hide');
+		console.log('classList after?', dlg.classList);
+
+	});
+
 });
--- static/js/functions.js
+++ static/js/functions.js
@@ -25,40 +25,41 @@ async function doLogin(event){
 	var password = event.target.password.value;
 	if(doLoginIsValidForm(login, password)){
 
-	var loginResult = await new Promise(function(resolve){
+		var loginResult = await new Promise(function(resolve){
 
-		fetch('/api/login', {
-	    	method: 'POST',
-	    	headers: {
-	    		'Content-Type': 'application/json;charset=utf-8'
-	    	},
-	    	body: JSON.stringify({login: login, password: password})
-	    }).then(function(response){
-		if(response.ok){
-		    response.text().then(function(data){
-			resolve({status: 'success', data: data});
-		    });
+			fetch('/api/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json;charset=utf-8'
+				},
+				body: JSON.stringify({login: login, password: password})
+			}).then(function(response){
+				if(response.ok){
+					response.text().then(function(data){
+						resolve({status: 'success', data: data});
+					});
+				} else {
+					var notOkResponse = `${response.status} ${response.statusText}`;
+					resolve({status: 'error', data: notOkResponse});
+				}
+			}).catch(function(error){
+				resolve({status: 'error', data: error});
+			});
+
+		});
+
+		if(loginResult.status === 'success'){
+			var dataJSON = JSON.parse(loginResult.data);
+			localStorage.setItem('token', dataJSON.token);
+			localStorage.setItem('login', dataJSON.login);
+			document.getElementById('user-name-area').innerHTML = `Вы вошли как: <b>${dataJSON.login}</b>`;
+			document.location.hash='#list';
 		} else {
-			var notOkResponse = `${response.status} ${response.statusText}`;
-		    resolve({status: 'error', data: notOkResponse});
+			document.getElementById('login-form-errors').innerHTML = loginResult.data;
+			setTimeout(function(){
+				document.getElementById('login-form-errors').innerHTML = '';
+			}, 3000);
 		}
-	    }).catch(function(error){
-			resolve({status: 'error', data: error});
-	    });
-
-	});
-
-	if(loginResult.status === 'success'){
-		console.log('DO WITH TOKEN!!!', loginResult.data);
-		var dataJSON = JSON.parse(loginResult.data);
-		localStorage.setItem('token', dataJSON.token);
-		document.location.hash='#list';
-	} else {
-		document.getElementById('login-form-errors').innerHTML = loginResult.data;
-		setTimeout(function(){
-			document.getElementById('login-form-errors').innerHTML = '';
-		}, 3000);
-	}
 
 	}
 }
@@ -68,120 +69,241 @@ async function doLogout(){
 	var logoutResult = await new Promise(function(resolve){
 
 		fetch('/api/logout', {
-	    	method: 'POST',
-	    	headers: {
-	    		'Content-Type': 'application/json;charset=utf-8'
-	    	},
-	    	body: JSON.stringify({token: token})
-	    }).then(function(response){
-		if(response.ok){
-		    response.text().then(function(data){
-			resolve({status: 'success', data: data});
-		    });
-		} else {
-			var notOkResponse = `${response.status} ${response.statusText}`;
-		    resolve({status: 'error', data: notOkResponse});
-		}
-	    }).catch(function(error){
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8'
+			},
+			body: JSON.stringify({token: token})
+		}).then(function(response){
+			if(response.ok){
+				response.text().then(function(data){
+					resolve({status: 'success', data: data});
+				});
+			} else {
+				var notOkResponse = `${response.status} ${response.statusText}`;
+				resolve({status: 'error', data: notOkResponse});
+			}
+		}).catch(function(error){
 			resolve({status: 'error', data: error});
-	    });
+		});
 
 	});
 
 	if(logoutResult.status === 'success'){
 		localStorage.removeItem('token');
+		localStorage.removeItem('login');
+		document.getElementById('login-form-errors').innerHTML = '';
 		document.location.hash='#login';
 	}
 }
 
 async function login(){
-    try {
-	document.getElementById("app").innerHTML = await new Promise(function(resolve){
-		fetch(`/views/view-login.html`).then(function(response){
-		if(response.ok){
-		    response.text().then(function(data){
-			resolve(data);
-		    });
-		} else {
-		    resolve('ERROR DETECTED');
-		}
-	    }).catch(function(error){
-		resolve('ERROR DETECTED ' + error);
-	    });
-	});
-
-	if(document.location.hash === '#login'){
-		var loginForm = document.getElementById('login-form');
-		loginForm.addEventListener('submit', function(event){
-			event.preventDefault();
-			doLogin(event);
+	try {
+		document.getElementById("app").innerHTML = await new Promise(function(resolve){
+			fetch(`/views/view-login.html`).then(function(response){
+				if(response.ok){
+					response.text().then(function(data){
+						resolve(data);
+					});
+				} else {
+					resolve('ERROR DETECTED');
+				}
+			}).catch(function(error){
+				resolve('ERROR DETECTED ' + error);
+			});
 		});
-	}
-	
-	
-    } catch(e){
+
+		if(document.location.hash === '#login'){
+			var loginForm = document.getElementById('login-form');
+			loginForm.addEventListener('submit', function(event){
+				event.preventDefault();
+				doLogin(event);
+			});
+		}
+		
+		
+	} catch(e){
 	//
-    }
+}
 }
 
 async function list(){
 
-    try {
-	document.getElementById("app").innerHTML = await new Promise(function(resolve){
-	    fetch(`/views/view-list.html`).then(function(response){
-		if(response.ok){
-		    response.text().then(function(data){
-			resolve(data);
-		    });
-		} else {
-		    resolve('ERROR DETECTED');
+	try {
+		document.getElementById("app").innerHTML = await new Promise(function(resolve){
+			fetch(`/views/view-list.html`).then(function(response){
+				if(response.ok){
+					response.text().then(function(data){
+						resolve(data);
+					});
+				} else {
+					resolve('ERROR DETECTED');
+				}
+			}).catch(function(error){
+				resolve('ERROR DETECTED ' + error);
+			});
+		});
+
+		if(document.location.hash === '#list'){
+			downloadDataFromServer();
 		}
-	    }).catch(function(error){
-		resolve('ERROR DETECTED ' + error);
-	    });
-	});
 
-	if(document.location.hash === '#list'){
-		downloadDataFromServer();
-	}
-
-    } catch(e){
+	} catch(e){
 	//
-    }
-    
+}
+
 }
 
 async function help(){
-    try {
-	document.getElementById("app").innerHTML = await new Promise(function(resolve){
-	    fetch(`/views/view-help.html`).then(function(response){
-		if(response.ok){
-		    response.text().then(function(data){
-			resolve(data);
-		    });
-		} else {
-		    resolve('ERROR DETECTED');
-		}
-	    }).catch(function(error){
-		resolve('ERROR DETECTED ' + error);
-	    });
-	});
-    } catch(e){
+	try {
+		document.getElementById("app").innerHTML = await new Promise(function(resolve){
+			fetch(`/views/view-help.html`).then(function(response){
+				if(response.ok){
+					response.text().then(function(data){
+						resolve(data);
+					});
+				} else {
+					resolve('ERROR DETECTED');
+				}
+			}).catch(function(error){
+				resolve('ERROR DETECTED ' + error);
+			});
+		});
+	} catch(e){
 	//
-    }
+}
+}
+
+function showImageInModal(filePath, fileTitle){
+	// console.log(`Открываем модальный диалог с изображением ${filePath}`);
+	// FIXME:
+	var dlg = document.getElementById('modal-dialog');
+	var dlgImg = document.getElementById('modal-dialog-img');
+	var dlgTitle = document.getElementById('modal-dialog-title');
+
+	dlgTitle.innerHTML = fileTitle;
+	dlgImg.setAttribute('src', filePath);
+
+	console.log('classList before?', dlg.classList);
+	dlg.classList.remove('hide');
+	dlg.classList.add('show');
+	console.log('classList after?', dlg.classList);
+
+
 }
 
 function refresh(inputData){
-    var listTableBody = document.getElementById('list-table-body');
-    listTableBody.innerHTML = '';
+	var listTableBody = document.getElementById('list-table-body');
+	listTableBody.innerHTML = '';
 
-    inputData.forEach(function(oneElement){
-	listTableBody.innerHTML += `<tr>
-				<td class="c1"><input type="checkbox" name="chb-${oneElement.id}" title="выберите для действия"></td>
-				<td>${oneElement.title}</td>
-				<td class="c3"><button>Скачать</button></td>
-			</tr>`;
-    });
+	inputData.forEach(function(oneElement){
+		listTableBody.innerHTML += `<tr>
+		<td class="c1"><input type="checkbox" class="chb-select" name="chb-${oneElement.id}" title="выберите для действия"></td>
+		<td>${oneElement.title}</td>
+		<td class="c3"><button type="button" class="watch-btn" data-title="${oneElement.title}" data-path="${oneElement.filepath}">Просмотр</button></td>
+		</tr>`;
+	});
+
+
+	// отбираем все кнопки "Просмотр" таблицы
+	var buttons = document.getElementsByClassName('watch-btn');
+
+	// на каждой из отобранных кнопок удаляем слушатель события клика и регистрируем заново
+	for(var btn of buttons){
+		/**
+		 * функция коллбэк, реагирующая на событие лкика в кнопку "Просмотр"
+		 **/
+		 function clickHandler(event){
+		 	var filePath = event.target.getAttribute('data-path');
+		 	var fileTitle = event.target.getAttribute('data-title');
+			showImageInModal(filePath, fileTitle); // 
+		}
+		btn.removeEventListener('click', clickHandler); // удаляем
+		btn.addEventListener('click', clickHandler); // регистрируем заново
+	}
+
+	// отобрать форму пакетной обработки данных
+	var batchActionsForm = document.getElementById('batch-actions-form');
+
+	function submitHandler(event){
+		event.preventDefault();
+		var form = event.target;
+
+		var formData = new FormData(form);
+		var batchIds = [];
+		var action;
+
+		for (var pair of formData.entries()) {
+			if(pair[0] === 'action'){
+				action = pair[1];
+			}
+			if((pair[0]).match(/^chb-/)){
+				batchIds.push((pair[0]).replace(/^chb-/, ''));
+			}
+		}
+
+		if(!batchIds.length){
+			alert('Нужно выбрать хотябы одну запись');
+			return;
+		}
+
+		console.log('action?', action);
+		console.log('batchIds?', batchIds);
+
+		var token = localStorage.getItem('token');
+		var baseUrl = document.location.toString().split("/#")[0];
+
+		switch (action){
+			case 'delete':
+				var url = new URL(`${baseUrl}/api/remove`);
+				url.searchParams.set('ids', batchIds);
+				fetch(url.toString(), {
+					method: 'delete',
+					headers: {
+						'token': token
+					}
+				}).then(function(response){
+					if(response.ok){
+						refresh(data);
+					} else {
+						var notOkResponse = `${response.status} ${response.statusText}`;
+						alert(notOkResponse);
+					}
+				}).catch(function(error){
+					alert('Network Error');
+				});
+			
+			break;
+
+			case 'download':
+				var url = new URL(`${baseUrl}/api/download`);
+				url.searchParams.set('ids', batchIds);
+				fetch(url.toString(), {
+					headers: {
+						'token': token
+					}
+				}).then(function(response){
+					if(response.ok){
+						refresh(data);
+					} else {
+						var notOkResponse = `${response.status} ${response.statusText}`;
+						alert(notOkResponse);
+					}
+				}).catch(function(error){
+					alert('Network Error');
+				});
+			break;
+
+			case 'select':
+				alert('Не выбрано действие');
+			break;
+		}
+
+	}
+
+	// удалить существующий слушатель события отправки формы и зарегистрировать его заново
+	batchActionsForm.removeEventListener('submit', submitHandler);
+	batchActionsForm.addEventListener('submit', submitHandler);
 }
 
 function downloadDataFromServer(){
@@ -191,23 +313,22 @@ function downloadDataFromServer(){
     var listTableBody = document.getElementById('list-table-body');
     listTableBody.innerHTML = `<tr><td colspan=3>Please wait... Loading....</td></tr>`;
     fetch(`/api/get-list`, {
-	headers: {
-	    'token': token
-	}
+    	headers: {
+    		'token': token
+    	}
     }).then(function(response){
-	if(response.ok){
-	    response.json().then(function(data){
-		refresh(data);
-	    });
-	} else {
-	    var notOkResponse = `${response.status} ${response.statusText}`;
-	    listTableBody.innerHTML = `<tr><td colspan=3><div class="error">${notOkResponse}</div></td></tr>`;
-	    setTimeout(function(){
-			document.location.hash='#login';
-	    }, 1500);
-	}
+    	if(response.ok){
+    		response.json().then(function(data){
+    			refresh(data);
+    		});
+    	} else {
+    		var notOkResponse = `${response.status} ${response.statusText}`;
+    		listTableBody.innerHTML = `<tr><td colspan=3><div class="error">${notOkResponse}</div></td></tr>`;
+    		setTimeout(function(){
+    			document.location.hash='#login';
+    		}, 1500);
+    	}
     }).catch(function(error){
-	listTableBody.innerHTML = `<tr><td colspan=3><div class="error">Network Error (await 5 sec...)</div></td></tr>`;
+    	listTableBody.innerHTML = `<tr><td colspan=3><div class="error">Network Error (await 5 sec...)</div></td></tr>`;
     });
 }
-
